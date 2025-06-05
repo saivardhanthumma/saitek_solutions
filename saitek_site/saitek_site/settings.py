6668c73d6c48047b397880ca2bd5331e4e108a20
thumma saivardhan <103802089+saivardhanthumma@users.noreply.github.com>
2025-06-05 18:34:57 +0530

    Update settings.py

--- saitek_site/saitek_site/settings.py
+++ saitek_site/saitek_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m5@*cfp%@-rcskmpkiyu9mz&&$s8&%gxbj7qntwtktsmjw2%6('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
